0899222a2e48d0589d77385ee177dba34ceed904
jlkesh <john.lgd65@gmail.com>
2022-08-04 10:06:23 +0500

    go go

---
+++ src/main/java/uz/jl/spring_mvc/HomeController.java
@@ -0,0 +1,17 @@
+package uz.jl.spring_mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String homePage() {
+        return "Hello survive";
+    }
+
+}
